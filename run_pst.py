bd47f0bbd3fc32b3332e95f55104dc0c8d5508cd
cynetics <brycargue@gmail.com>
2021-02-09 19:12:16 +0100

    changed the dim_in to a representable dimension for our problem

--- run_pst.py
+++ run_pst.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 def gen_data(batch_size, max_length=10, test=False):
     x = np.random.randint(1, 100, (batch_size, max_length, 5))
-    y = np.max(x, axis=1)
+    y = np.max(x, axis=1)[:,0]
 
     return x, y
 
@@ -16,7 +16,7 @@ class SmallSetTransformer(nn.Module):
         super().__init__()
         print("Set Transformer Model Initialized")
         self.enc = nn.Sequential(
-            SAB(dim_in=1, dim_out=64, num_heads=4),
+            SAB(dim_in=5, dim_out=64, num_heads=4),
             SAB(dim_in=64, dim_out=64, num_heads=4),
         )
         self.dec = nn.Sequential(
@@ -37,12 +37,12 @@ def train(model):
     losses = []
     for _ in range(500):
         x, y = gen_data(batch_size=2 ** 10, max_length=10)
-        x, y = torch.from_numpy(x).float().cuda(), torch.from_numpy(y).float().cuda()
+        x, y = torch.from_numpy(x).float().cuda(), torch.from_numpy(y).float().cuda().unsqueeze(1)
 
         output = model(x)
-        print("output shape: ", output.shape)
-        print("y shape: ", y.shape)
-        raise ValueError()
+        #print("output shape: ", output.shape)
+        #print("y shape: ", y.shape)
+        #raise ValueError()
         loss = criterion(output, y)
         optimizer.zero_grad()
         loss.backward()
