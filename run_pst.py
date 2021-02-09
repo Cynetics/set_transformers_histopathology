c6d8a503829fdf2e597d822e6a28582c48b9cf1f
cynetics <brycargue@gmail.com>
2021-02-09 18:35:14 +0100

    set transformer for histopathology base file

---
+++ run_pst.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+from modules import SAB, PMA
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def gen_data(batch_size, max_length=10, test=False):
+    length = np.random.randint(1, max_length + 1)
+    x = np.random.randint(1, 100, (batch_size, length))
+    y = np.max(x, axis=1)
+    x, y = np.expand_dims(x, axis=2), np.expand_dims(y, axis=1)
+    print("x: ", x)
+    print("y: ", y)
+    print(x.shape)
+    print(y.shape)
+    raise ValueError()
+    return x, y
+
+class SmallSetTransformer(nn.Module):
+    def __init__(self,):
+        super().__init__()
+        print("Set Transformer Model Initialized")
+        self.enc = nn.Sequential(
+            SAB(dim_in=1, dim_out=64, num_heads=4),
+            SAB(dim_in=64, dim_out=64, num_heads=4),
+        )
+        self.dec = nn.Sequential(
+            PMA(dim=64, num_heads=4, num_seeds=1),
+            nn.Linear(in_features=64, out_features=1),
+        )
+
+    def forward(self, x):
+        x = self.enc(x)
+        x = self.dec(x)
+        return x.squeeze(-1)
+
+
+def train(model):
+    model = model.cuda()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    criterion = nn.L1Loss().cuda()
+    losses = []
+    for _ in range(500):
+        x, y = gen_data(batch_size=2 ** 10, max_length=10)
+        x, y = torch.from_numpy(x).float().cuda()., torch.from_numpy(y).float().cuda()
+        loss = criterion(model(x), y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.item())
+    return losses
+
+models = [("Set Transformer", SmallSetTransformer())]
+
+for _name, _model in models:
+    _losses = train(_model)
+    plt.plot(_losses, label=_name)
+plt.legend()
+plt.xlabel("Steps")
+plt.ylabel("Mean Absolute Error")
+plt.yscale("log")
+plt.show()
