f0928cb8b3e820e0bc5ee1978564c1d86d3afe2b
Bryan Cardenas <brycargue@gmail.com>
2021-02-10 21:40:00 +0100

    standardizing the train file for ease of use

--- run_pst.py
+++ run_pst.py
@@ -5,6 +5,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 def gen_data(batch_size, max_length=10, test=False):
     x = np.random.randint(1, 100, (batch_size, max_length, 5))
     y = np.max(x, axis=1)[:,0]
@@ -30,14 +32,15 @@ class SmallSetTransformer(nn.Module):
         return x.squeeze(-1)
 
 
-def train(model):
+def train(config, training_loader):
     model = model.cuda()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
     criterion = nn.L1Loss().cuda()
+    # TODO: add classweights
+    criterion = nn.CrossEntropyLoss()
     losses = []
-    for _ in range(500):
-        x, y = gen_data(batch_size=2 ** 10, max_length=10)
-        x, y = torch.from_numpy(x).float().cuda(), torch.from_numpy(y).float().cuda().unsqueeze(1)
+    for x, y in training_loader:
+        x, y = x.to(device), y.to(device).long()
 
         output = model(x)
         #print("output shape: ", output.shape)
@@ -48,9 +51,22 @@ def train(model):
         loss.backward()
         optimizer.step()
         losses.append(loss.item())
+        if (i+1) % config.print_every==0:
+            print("[==========================]")
+            print("Current Iteration: {}".format(i+1))
+            print('Total Loss: %.4f' % np.mean(total_l[-100:]))
+            print('classification Loss: %.4f' % np.mean(class_l[-100:]))
+            print('Segmentation Loss: %.4f' % np.mean(seg_l[-100:]))
+            print("Learning Rate: ", optimizer.param_groups[0]['lr'])
+            if config.validate: 
+                dice = test_net(net, val_loader,val_num=config.val_num)
+                save_model(net,dir_checkpoint + model_name + str(dice) + '_{}.pth'.format(i), save_model=save_cp)             
+                print("checkpoint saved")
+
+            net.train()
     return losses
 
-models = [("Set Transformer", SmallSetTransformer())]
+models = [("Set Transformer", )]
 
 for _name, _model in models:
     _losses = train(_model)
@@ -60,3 +76,30 @@ plt.xlabel("Steps")
 plt.ylabel("Mean Absolute Error")
 plt.yscale("log")
 plt.show()
+
+def main():
+
+    config = set_transformers_config("testing phase")
+
+    model = SmallSetTransformer()
+    # Load Data
+    training_data = get_transformer_data(config)
+    val_data = get_transformer_val_data(config)
+
+    training_loader = DataLoader(training_data,
+                             batch_size=config.batch_size,
+                             shuffle=True,
+                             drop_last=False)
+
+    model = nn.DataParallel(model).to(device)
+    if config.model_checkpoint is not None:
+        with open(config.model_checkpoint, 'rb') as f:
+                state_dict = torch.load(f)
+                model.load_state_dict(state_dict)
+                print("Model Loaded!")
+            
+    model.train()
+    train(model, training_loader, training_data)
+
+if __name__ == '__main__':
+    main()
